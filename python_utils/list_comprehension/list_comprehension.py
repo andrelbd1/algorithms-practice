90fa924cf1d0a681e8739abddee5195f6d94d556
Andre Damasceno <andrelbd1@gmail.com>
2024-09-22 21:19:35 -0300

    Adding .py file to list_comprehension

---
+++ python_utils/list_comprehension/list_comprehension.py
@@ -0,0 +1,18 @@
+# List Comprehension
+## List comprehension in Python are constructed as follows:
+lst = [i for i in range(0,6)]
+print(lst)
+
+## Sum 2 in each list item
+res = []
+for i in lst:
+    res.append(i+2)
+print(res)
+
+## Sum 2 in each list item using list comprehensions
+res = [i+2 for i in lst]
+print(res)
+
+## Sum 2 in each even list item using list comprehensions
+res = [i+2 if i%2==0 and i!=0 else i for i in lst]
+print(res)
