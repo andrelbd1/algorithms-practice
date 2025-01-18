85af62ef49f8f818692854610c7b27c065034777
Andre Damasceno <andrelbd1@gmail.com>
2025-01-18 17:14:55 -0300

    Improving map  examples

--- python_utils/map/example_1.py
+++ python_utils/map/example_1.py
@@ -1,11 +1,29 @@
+""" Applying TAX """
 prices = [1.09, 23.56, 57.84, 4.56, 6.78]
-TAX_RATE = .08
+TAX_RATE = 8
 
 
-def get_price_with_tax(price):
-    return price * (1 + TAX_RATE)
+def get_price_with_tax(prices, tax):
+    return map(lambda p: p * (1 + tax / 100), prices)
 
 
-final_prices = map(get_price_with_tax, prices)
+final_prices = get_price_with_tax(prices, TAX_RATE)
 final_prices = list(final_prices)
-print(final_prices)
+print("Applying TAX")
+print(f"\tPrices: {prices}")
+print(f"\tPrices with tax ({TAX_RATE}%): {final_prices}\n")
+
+""" Applying DISCOUNT """
+prices = [100, 200, 300, 400, 500]
+DISCOUNT_RATE = 15
+
+
+def get_price_with_discount(prices, discount):
+    return map(lambda p: p * (1 - discount / 100), prices)
+
+
+final_prices = get_price_with_discount(prices, DISCOUNT_RATE)
+final_prices = list(final_prices)
+print("Applying DISCOUNT")
+print(f"\tPrices: {prices}")
+print(f"\tPrices with discount ({DISCOUNT_RATE}%): {final_prices}\n")
