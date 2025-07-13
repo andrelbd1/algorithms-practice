ed70882ad0177e264ee4e459918f8e0f8f993f0a
André Brandão <andrelbd1@gmail.com>
2025-07-13 13:44:00 -0700

    Improving filter description

--- python_utils/filter/README.md
+++ python_utils/filter/README.md
@@ -1,6 +1,10 @@
 ### Filter function
-- The filter() function constructs an iterator from elements of an iterable for which a function returns true.
+- The `filter()` allows you to filter items from an iterable based on evaluation of the given function. It applies function  to each element of and returns an iterator that yields all items for which is truthy. Conversely, it filters out all items for which is falsy.
 
+
+````bash
+    filter(<f>, <iterable>)
+````
 
 #### Examples
 - [Code](example_1.py)
--- python_utils/filter/example_1.py
+++ python_utils/filter/example_1.py
@@ -1,14 +1,7 @@
 sales = [100, 200, 50, 300, 150]
 threshold = 150
 
-
-def filter_high_sales(sales, threshold):
-    """Filter sales greater than the threshold"""
-    return filter(lambda s: s > threshold, sales)
-
-
-sales_filtered = filter_high_sales(sales, threshold)
-sales_filtered = list(sales_filtered)
+sales_filtered = filter(lambda s: s > threshold, sales)
 print("Applying filter")
-print(f"\tSales: {sales}")
-print(f"\tSales with threshold ({threshold}): {sales_filtered}\n")
+print(f"\tSales: {sales}")  # [100, 200, 50, 300, 150]
+print(f"\tSales with threshold ({threshold}): {list(sales_filtered)}\n")  # (150): [200, 300]
